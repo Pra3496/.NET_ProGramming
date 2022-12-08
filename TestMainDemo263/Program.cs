6bd032a2cd4df3954a4a73c61a3106c6b27eee75
Pranav Waghmare <piyushw55@gmail.com>
2022-12-08 20:42:46 +0530

    Day4 Static Function

--- TestMainDemo263/Program.cs
+++ TestMainDemo263/Program.cs
@@ -4,28 +4,41 @@ namespace TestMainDemo263
 {
     internal class Program
     {
+
+        public static void myMethod()
+        {
+            Console.WriteLine(" hello, World ");
+        }
+
+        public static void myMethod(int a, int b)
+        {
+            Console.WriteLine("Sum is " + (a + b));
+        }
         static void Main(string[] args)
         {
-            int fir = 0;
-            int sec = 0;
+            //int fir = 0;
+            //int sec = 0;
 
-            Console.WriteLine("Calculator Class -> Addition");
-            Calculator cal = new Calculator();
+           // Console.WriteLine("Calculator Class -> Addition");
+           // Calculator cal = new Calculator();
 
-            Console.WriteLine("Enter the First Number :");
-            fir = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Enter the First Number :");
+            // fir = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter the Second Number :");
-            sec = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Enter the Second Number :");
+            // sec = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Sum of {0} and {1} is {2}", fir, sec, cal.Addition(fir, sec));
+            // Console.WriteLine("Sum of {0} and {1} is {2}", fir, sec, cal.Addition(fir, sec));
 
-            Console.WriteLine("Enter the User Name :");
-            String userName = Console.ReadLine();
-            Console.WriteLine("Enter the Batch Name :");
-            String Batch = Console.ReadLine();
+            // Console.WriteLine("Enter the User Name :");
+            // String userName = Console.ReadLine();
+            // Console.WriteLine("Enter the Batch Name :");
+            // String Batch = Console.ReadLine();
 
-            Console.WriteLine("Hello,{0}\n\tBatch Name is {1}", userName, Batch);
+            // Console.WriteLine("Hello,{0}\n\tBatch Name is {1}", userName, Batch);
+
+            myMethod();
+            myMethod(1,2);
 
         }
     }
